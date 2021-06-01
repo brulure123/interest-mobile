cb16db842932a98aa91af6c9def0574820012f69
brulure123 <benraj.200@gmail.com>
2021-06-01 17:11:14 +0200

    Ajout de l'UI de la page d'enregistrement. PROCHAINE ETAPE mis en place du service pour authentification avec le serveur.

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  showPassword = false;
+  showConfPassword = false;
+
+  registerForm: FormGroup = new FormGroup({
+    pseudo: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    conf_password: new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  doRegister() {
+    console.log('Do register');
+  }
+
+  togglePasswordFieldType() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfPasswordFieldType() {
+    this.showConfPassword = !this.showConfPassword;
+  }
 }
---
+++ src/app/auth/services/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+// TODO: Implementation du service d'authentification des que le serveurs sera prêt
+export class AuthService {
+
+  constructor() { }
+}
